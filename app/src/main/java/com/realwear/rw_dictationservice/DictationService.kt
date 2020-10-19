3738ffd7f62676f582e7cb8203d7afba8930683c
Jonathan Barrera <jonathan.barrera@realwear.com>
2020-10-19 15:53:34 -0700

    Test with continuous dictation.

--- app/src/main/java/com/realwear/rw_dictationservice/DictationService.kt
+++ app/src/main/java/com/realwear/rw_dictationservice/DictationService.kt
@@ -4,71 +4,124 @@ import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognitionService
 import android.util.Log
-import com.microsoft.cognitiveservices.speech.*
+import com.microsoft.cognitiveservices.speech.SpeechConfig
+import com.microsoft.cognitiveservices.speech.SpeechRecognitionEventArgs
+import com.microsoft.cognitiveservices.speech.SpeechRecognitionResult
+import com.microsoft.cognitiveservices.speech.SpeechRecognizer
 import com.microsoft.cognitiveservices.speech.audio.AudioConfig
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
 class DictationService : RecognitionService() {
-    private val TAG = DictationService::class.java.simpleName
+    private lateinit var mSpeechRecognizer: SpeechRecognizer
     private lateinit var mCallback: Callback
-    private val SpeechSubscriptionKey = "226314cc4670432e87d9f80f805bffea"
-    private val SpeechRegion = "westus2"
     private val s_executorService = Executors.newCachedThreadPool()
-
-    override fun onCreate() {
-        super.onCreate()
-        Log.i(TAG, "Jonathan Service started")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(TAG, "Jonathan Service stopped")
-    }
 
     override fun onStartListening(intent: Intent, callback: Callback) {
         Log.d(TAG, "jonathan start listening")
 
         mCallback = callback
         val extras = intent.extras ?: Bundle()
+        val dictationType = extras.get(EXTRA_DICTATION_TYPE)
         mCallback.readyForSpeech(extras)
 
-        startDictation()
+        when (dictationType) {
+            ASYNC_DICTATION -> startAsyncDictation()
+            CONTINUOUS_DICTATION -> startContinuousDictation()
+            else -> startAsyncDictation()
+        }
     }
 
-    private fun startDictation() {
+    /**
+     * Method to start Microsoft dictation service.
+     */
+    private fun startAsyncDictation() {
         val speechConfig =
             SpeechConfig.fromSubscription(SpeechSubscriptionKey, SpeechRegion)
-
         val audioInput = AudioConfig.fromDefaultMicrophoneInput()
+        mSpeechRecognizer = SpeechRecognizer(speechConfig, audioInput)
+        val task: Future<SpeechRecognitionResult> = mSpeechRecognizer.recognizeOnceAsync()
 
-        val speechRecognizer = SpeechRecognizer(speechConfig, audioInput)
+        mSpeechRecognizer.recognizing.addEventListener { _: Any?, speechRecognitionResultEventArgs: SpeechRecognitionEventArgs ->
+            val text = speechRecognitionResultEventArgs.result.text
+            val bundle = Bundle()
+            val arrayList = ArrayList<String>()
+            arrayList.add(text)
+            bundle.putStringArrayList(
+                android.speech.SpeechRecognizer.RESULTS_RECOGNITION,
+                arrayList
+            )
+            mCallback.partialResults(bundle)
+        }
 
-        val task: Future<SpeechRecognitionResult> = speechRecognizer.recognizeOnceAsync()
         setOnTaskCompletedListener(task,
             object : OnTaskCompletedListener<SpeechRecognitionResult> {
                 override fun onCompleted(taskResult: SpeechRecognitionResult) {
-                    val text = taskResult.text
-                    Log.d(TAG, "jonathan results: " + text)
-                    speechRecognizer.close()
-                    onStopListening(mCallback)
+                    mCallback.endOfSpeech()
 
+                    val text = taskResult.text
                     val bundle = Bundle()
                     val arrayList = ArrayList<String>()
                     arrayList.add(text)
-                    bundle.putStringArrayList(android.speech.SpeechRecognizer.RESULTS_RECOGNITION, arrayList)
+                    bundle.putStringArrayList(
+                        android.speech.SpeechRecognizer.RESULTS_RECOGNITION,
+                        arrayList
+                    )
                     mCallback.results(bundle)
                 }
             })
     }
 
-    override fun onStopListening(callback: Callback) {
-        Log.d(TAG, "jonathan stop listening")
+    private fun startContinuousDictation() {
+        val speechConfig =
+            SpeechConfig.fromSubscription(SpeechSubscriptionKey, SpeechRegion)
+        val audioInput = AudioConfig.fromDefaultMicrophoneInput()
+        mSpeechRecognizer = SpeechRecognizer(speechConfig, audioInput)
+        val content = java.util.ArrayList<String>()
+        mSpeechRecognizer.recognizing.addEventListener { _, speechRecognitionEventArgs ->
+            val text: String = speechRecognitionEventArgs.result.text
+            content.add(text)
+
+            val bundle = Bundle()
+            bundle.putStringArrayList(
+                android.speech.SpeechRecognizer.RESULTS_RECOGNITION,
+                content
+            )
+            mCallback.partialResults(bundle)
+
+            content.removeAt(content.size - 1)
+        }
+
+        mSpeechRecognizer.recognized.addEventListener { _, speechRecognitionEventArgs ->
+            val text: String = speechRecognitionEventArgs.result.text
+            content.add(text)
+
+            val bundle = Bundle()
+            bundle.putStringArrayList(
+                android.speech.SpeechRecognizer.RESULTS_RECOGNITION,
+                content
+            )
+            mCallback.results(bundle)
+        }
+
+        mSpeechRecognizer.startContinuousRecognitionAsync()
+    }
+
+    override fun onStopListening(p0: Callback?) {
+        Log.d(TAG, "onStopListening called.")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy called.")
+        mSpeechRecognizer.stopContinuousRecognitionAsync()
+        mSpeechRecognizer.close()
+
+        super.onDestroy()
     }
 
     override fun onCancel(p0: Callback?) {
-        Log.d(TAG, "Jonathan recog Service Callback Canceled.")
+        Log.d(TAG, "onCancel called.")
     }
 
     private fun <T> setOnTaskCompletedListener(
@@ -87,4 +140,12 @@ class DictationService : RecognitionService() {
         fun onCompleted(taskResult: T)
     }
 
+    companion object {
+        private val TAG = DictationService::class.java.simpleName
+        private const val SpeechSubscriptionKey = "226314cc4670432e87d9f80f805bffea"
+        private const val SpeechRegion = "westus2"
+        private const val EXTRA_DICTATION_TYPE = "extra_dictation_type"
+        private const val ASYNC_DICTATION = 0
+        private const val CONTINUOUS_DICTATION = 1
+    }
 }
